18832ecc5cc127e7202048b75b61b03152aac0b8
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-06-19 08:35:21 +0000

    added the main page component route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { MainpageComponent } from './mainpage/mainpage.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainpageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
