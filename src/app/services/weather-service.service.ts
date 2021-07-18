a3248cafd11a87216562f55afbb2a6e1085bbf68
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-07-18 11:17:17 +0000

    added a getCurrentWeather funnction to get the current weather

--- src/app/services/weather-service.service.ts
+++ src/app/services/weather-service.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class WeatherServiceService {
-  APIkey = 'this is my api key and he is ok';
+  APIkey = '';
   URL = 'http://api.openweathermap.org/data/2.5/forecast?q=';
 
   constructor(private http: HttpClient) { }
@@ -14,6 +14,6 @@ export class WeatherServiceService {
 
   // get the current weather based on the city
   getCurrentWeather(city) {
-    return this.http.get(this.URL + city + '&APPID=' + this.APIkey)
+    return this.http.get(`${this.URL}/weather?q=${city}&appid=${this.APIkey}`)
   }
 }
