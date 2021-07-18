23ba429bf01ffd6ebf51378a4a3ce406c170f284
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-07-18 11:07:41 +0000

    created a weather service

--- src/app/services/weather-service.service.ts
+++ src/app/services/weather-service.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherServiceService {
+  APIkey = 'this is my api key and he is ok';
+  URL = 'http://api.openweathermap.org/data/2.5/forecast?q=';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  // get the current weather based on the city
+  getCurrentWeather(city) {
+    return this.http.get(this.URL + city + '&APPID=' + this.APIkey)
+  }
 }
