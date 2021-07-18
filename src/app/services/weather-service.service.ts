9e5232e6d463cdfdac4a23218d2dea0357df2250
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-07-18 11:21:30 +0000

    added a getCurrentWeather funnction to get the current weather

--- src/app/services/weather-service.service.ts
+++ src/app/services/weather-service.service.ts
@@ -6,14 +6,14 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class WeatherServiceService {
-  APIkey = '';
-  URL = 'http://api.openweathermap.org/data/2.5/forecast?q=';
+  APIkey:string = 'b23e6a0dcfmsh4ad3478f38e8989p1bea08jsn7bcc14a25fc6';
+  URL:string = 'https://community-open-weather-map.p.rapidapi.com';
 
   constructor(private http: HttpClient) { }
 
 
   // get the current weather based on the city
   getCurrentWeather(city) {
-    return this.http.get(`${this.URL}/weather?q=${city}&appid=${this.APIkey}`)
+    return this.http.get(`${this.URL}/weather?q=${city}&appid=${this.APIkey}`);
   }
 }
