2e5bfacf54fd112db223ef9e626c48eb4c14b19f
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-07-25 00:03:05 +0000

    added a function to convert the date and time values to standard dates

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -27,4 +27,25 @@ export class WeatherService {
   getClimateForecast(city: string) {
     return this.http.get(`${this.URL}/climate?q=${city}&appid=${this.APIkey}`)
   }
+
+  //This is to change the unix date to a standard date
+  convertUnixDate(date, dayname = false, time = false){
+    const day = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
+    const month = ["Jan", "Feb", "Mar", "April", "May", "June",
+        "July", "Aug", "Sept", "Oct", "Nov", "Dec"
+    ];
+
+    const d = new Date(date * 1000)
+    const isAMorPM = d.getHours() >= 12 ? "pm" : "am"
+    const weatherDateTime = `${d.getHours() > 12 ? d.getHours() - 12 : d.getHours()}:${d.getMinutes()} ${isAMorPM}, ${day[d.getDay()]}, ${month[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`
+    const dayName = `${month[d.getMonth()]} ${d.getDate()}`
+    const getTime = `${d.getHours() > 12 ? d.getHours() - 12 : d.getHours()}:${d.getMinutes() > 9 ? d.getMinutes() : "0" + d.getMinutes()} ${isAMorPM}`
+    if (dayname)
+        return dayName
+
+    if (time)
+        return getTime
+
+    return weatherDateTime
+}
 }
