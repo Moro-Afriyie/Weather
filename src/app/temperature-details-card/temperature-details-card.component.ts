44efabc143d10ccc565053a34654d6d28a1389c0
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-07-25 00:04:45 +0000

    added a function to convert the date and time values to standard dates

--- src/app/temperature-details-card/temperature-details-card.component.ts
+++ src/app/temperature-details-card/temperature-details-card.component.ts
@@ -50,39 +50,18 @@ export class TemperatureDetailsCardComponent implements OnInit {
     this.pressure = weather.main.pressure;
     this.humidity = weather.main.humidity;
     //sys
-    this.sunRise = this.convertUnixDate(weather.sys.sunrise, false, true);
-    this.sunSet = this.convertUnixDate(weather.sys.sunset, false, true);
+    this.sunRise = this.weatherService.convertUnixDate(weather.sys.sunrise, false, true);
+    this.sunSet = this.weatherService.convertUnixDate(weather.sys.sunset, false, true);
     //weather
     this.weatherDescription = weather.weather[0].description;
     // wind
     this.windSpeed =  (weather.wind.speed * 1.609).toFixed(1);
     //date
-    console.log(this.convertUnixDate(weather.dt));
+    console.log(this.weatherService.convertUnixDate(weather.dt));
     //icon
     this.weatherIcon = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
     
   }
-
-  //This is to change the unix date to a standard date
-  convertUnixDate(date, dayname = false, time = false){
-        const day = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
-        const month = ["Jan", "Feb", "Mar", "April", "May", "June",
-            "July", "Aug", "Sept", "Oct", "Nov", "Dec"
-        ];
-
-        const d = new Date(date * 1000)
-        const isAMorPM = d.getHours() >= 12 ? "pm" : "am"
-        const weatherDateTime = `${d.getHours() > 12 ? d.getHours() - 12 : d.getHours()}:${d.getMinutes()} ${isAMorPM}, ${day[d.getDay()]}, ${month[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`
-        const dayName = `${month[d.getMonth()]} ${d.getDate()}`
-        const getTime = `${d.getHours() > 12 ? d.getHours() - 12 : d.getHours()}:${d.getMinutes() > 9 ? d.getMinutes() : "0" + d.getMinutes()} ${isAMorPM}`
-        if (dayname)
-            return dayName
-
-        if (time)
-            return getTime
-
-        return weatherDateTime
-    }
 
     
 }
