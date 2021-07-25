813bd4f7060ab1b9cfe65ab7d9706c650251f5af
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-07-25 00:01:07 +0000

    done with the temperature details component

--- src/app/temperature-details-card/temperature-details-card.component.ts
+++ src/app/temperature-details-card/temperature-details-card.component.ts
@@ -18,7 +18,7 @@ export class TemperatureDetailsCardComponent implements OnInit {
   humidity: number;
   pressure: string;
   temperature: string;
-  mininumTemperature: string;
+  minimumTemperature: string;
   maximumTemperature: string;
   //sys
   sunRise: string;
@@ -27,7 +27,8 @@ export class TemperatureDetailsCardComponent implements OnInit {
   //weather
   weatherDescription: string;
   weatherIcon: string;
-  windSpeed: number;
+  //wind
+  windSpeed: string;
   
 
   constructor( private weatherService: WeatherService) { }
@@ -38,14 +39,50 @@ export class TemperatureDetailsCardComponent implements OnInit {
       this.getWeatherDetails(res);
     });
   }
+
   getWeatherDetails(weather:any){
     console.log("current weather + ", weather);
+    // main
     this.feelsLike = (weather.main.feels_like-273.15).toFixed(0);
     this.temperature = (weather.main.temp-273.15).toFixed(0);
-    this.mininumTemperature = (weather.temp_min-273.15).toFixed(0);
-    this.maximumTemperature = (weather.temp_max-273.15).toFixed(0);
+    this.minimumTemperature = (weather.main.temp_min-273.15).toFixed(0);
+    this.maximumTemperature = (weather.main.temp_max-273.15).toFixed(0);
     this.pressure = weather.main.pressure;
     this.humidity = weather.main.humidity;
+    //sys
+    this.sunRise = this.convertUnixDate(weather.sys.sunrise, false, true);
+    this.sunSet = this.convertUnixDate(weather.sys.sunset, false, true);
+    //weather
+    this.weatherDescription = weather.weather[0].description;
+    // wind
+    this.windSpeed =  (weather.wind.speed * 1.609).toFixed(1);
+    //date
+    console.log(this.convertUnixDate(weather.dt));
+    //icon
+    this.weatherIcon = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
+    
   }
 
+  //This is to change the unix date to a standard date
+  convertUnixDate(date, dayname = false, time = false){
+        const day = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
+        const month = ["Jan", "Feb", "Mar", "April", "May", "June",
+            "July", "Aug", "Sept", "Oct", "Nov", "Dec"
+        ];
+
+        const d = new Date(date * 1000)
+        const isAMorPM = d.getHours() >= 12 ? "pm" : "am"
+        const weatherDateTime = `${d.getHours() > 12 ? d.getHours() - 12 : d.getHours()}:${d.getMinutes()} ${isAMorPM}, ${day[d.getDay()]}, ${month[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`
+        const dayName = `${month[d.getMonth()]} ${d.getDate()}`
+        const getTime = `${d.getHours() > 12 ? d.getHours() - 12 : d.getHours()}:${d.getMinutes() > 9 ? d.getMinutes() : "0" + d.getMinutes()} ${isAMorPM}`
+        if (dayname)
+            return dayName
+
+        if (time)
+            return getTime
+
+        return weatherDateTime
+    }
+
+    
 }
